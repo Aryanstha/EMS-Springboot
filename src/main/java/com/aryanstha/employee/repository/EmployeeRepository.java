e535d296162ee895b13f841e31c85a5ee241ab88
Aryan Shrestha <aryanstha4859@gmail.com>
2023-08-14 19:03:42 +0545

    Add files via upload

---
+++ src/main/java/com/aryanstha/employee/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.aryanstha.employee.repository;
+
+import com.aryanstha.employee.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
